c9fa5b8cb07619af41f8e499cb1e5e3fc381c0c2
Asmit Rai <er.asmitrai@gmail.com>
2025-10-09 23:08:27 +0530

    cors updated

--- server.js
+++ server.js
@@ -10,7 +10,10 @@ dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3000;
-app.use(cors());
+const corsOptions = {
+  origin: 'https://flat-file-parser.vercel.app', 
+};
+app.use(cors(corsOptions));
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
 
